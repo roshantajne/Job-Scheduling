0a3cdc2ef3ddf581f602134b969595da93944e34
roshantajne <rtajne7012@gmail.com>
2024-02-28 06:58:39 +0530

    first commit

---
+++ project/src/Service.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Form from "./Form";
+import axios from 'axios';
+
+
+const Service = () => {
+
+
+  return (
+   <>
+   <Form/>
+   </>
+  );
+}
+
+export default Service;
