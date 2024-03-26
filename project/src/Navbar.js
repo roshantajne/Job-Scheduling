a240c681cdd4d4c21b4fcff2226f3a1098dfb370
roshantajne <96852514+roshantajne@users.noreply.github.com>
2024-03-26 08:04:35 +0530

    Update Navbar.js

--- project/src/Navbar.js
+++ project/src/Navbar.js
@@ -2,7 +2,7 @@ import React from "react";
 // import { NavLink } from "react-router-dom";
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
-import Navar1 from 'react-bootstrap/Navbar';
+import Navbar1 from 'react-bootstrap/Navbar';
 
 const Navbar = () => {
     return(
