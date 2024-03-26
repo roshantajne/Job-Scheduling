2a99ecc632ced5c636b4cc2e9a405fbad3424eb9
roshantajne <96852514+roshantajne@users.noreply.github.com>
2024-03-26 13:52:52 +0530

    Update Navbar.js

--- project/src/Navbar.js
+++ project/src/Navbar.js
@@ -11,9 +11,9 @@ const navbarNav = () => {
         <Container>
           <Navbar.Brand href="#home">Navbar</Navbar.Brand>
           <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#features">Features</Nav.Link>
-            <Nav.Link href="#pricing">Pricing</Nav.Link>
+            <Nav.Link href="/">Home</Nav.Link>
+            <Nav.Link href="/Service">Service</Nav.Link>
+            <Nav.Link href="/Details">Details</Nav.Link>
           </Nav>
         </Container>
       </Navbar>
