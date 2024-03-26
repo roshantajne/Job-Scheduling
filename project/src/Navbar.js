e6cba889cff46adef7e3fcf15d5dde4cec7a2e03
roshantajne <96852514+roshantajne@users.noreply.github.com>
2024-03-26 08:08:00 +0530

    Update Navbar.js

--- project/src/Navbar.js
+++ project/src/Navbar.js
@@ -4,7 +4,7 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 
-const Navbar = () => {
+const Navbar1 = () => {
     return(
         <>
         <Navbar1 bg="light" data-bs-theme="light">
@@ -21,4 +21,4 @@ const Navbar = () => {
     );
 }
 
-export default Navbar;
+export default Navbar1;
