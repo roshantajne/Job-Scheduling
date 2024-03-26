6803c8e3dd2eabb082bbeb4c071bfb3ccd0762ae
roshantajne <96852514+roshantajne@users.noreply.github.com>
2024-03-26 13:50:55 +0530

    Update Navbar.js

--- project/src/Navbar.js
+++ project/src/Navbar.js
@@ -1,47 +1,24 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
 
-const Navbar = () => {
+const navbarNav = () => {
     return(
         <>
-       
-  <div className="container-fluid nav-bg">
-    <div className="row">
-        <div className="col-10 mx-auto">
-
-            <nav className="navbar navbar-expand-lg navbar-light bg-light">
-                <div className="container-fluid">
-                  <NavLink className="navbar-brand" href="#">Fawara</NavLink>
-                  <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                    <span className="navbar-toggler-icon"></span>
-                  </button>
-                  <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                    <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
-                      <li className="nav-item">
-                        <NavLink activeClassName="menu_active" exact className="nav-link active" aria-current="page" to="/">Home</NavLink>
-                      </li>
-                      <li className="nav-item">
-                        <NavLink activeClassName="menu_active" className="nav-link" to="/Service">Service</NavLink>
-                      </li>
-                      
-                      <li className="nav-item">
-                        <NavLink activeClassName="menu_active" className="nav-link" to="/Details">Details</NavLink>
-                      </li>
-                      {/* <li className="nav-item">
-                        <NavLink activeClassName="menu_active" className="nav-link" to="/Contact">Contact</NavLink>
-                      </li> */}
-                      
-                    </ul>
-                   
-                  </div>
-                </div>
-              </nav>
-
-        </div>
-    </div>
-  </div>
+       <Navbar bg="light" data-bs-theme="light">
+        <Container>
+          <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#features">Features</Nav.Link>
+            <Nav.Link href="#pricing">Pricing</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
         </>
     );
 }
 
-export default Navbar;
+export default navbarNav;
